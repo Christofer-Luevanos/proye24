ddd26f6e6da8089b7f878570ef82938934755aac
unknown <Christofer>
2020-06-21 12:33:29 -0500

    cuarta version

--- dentilogin.php
+++ dentilogin.php
@@ -1,10 +1,11 @@
 <?php
   include 'denticonfig.php';
-  
-  $usua = filter_input(INPUT_POST, "usuario");
-  $password = filter_input(INPUT_POST, "contra");
-  $query = "SELECT * FROM usario WHERE nombre_usua = '". $usua."'
-            AND contraseña = '". $password ."'";
+  $usua ="fer";
+  $password= "12345";
+  //$usua = filter_input(INPUT_POST, "usuario");
+  //$password = filter_input(INPUT_POST, "contra");
+  $query = "SELECT * FROM usuario WHERE nombre= '". $usua."'
+            AND contra= '". $password ."'";
   $result = mysqli_query($conn, $query);
   $row = mysqli_fetch_array($result);
   if($row){
