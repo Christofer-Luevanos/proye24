ddce0bb9f91b43d11612c8c4d1fe6da7adf59a7a
Christofer-Luevanos <a17100195@gmail.com>
2020-06-21 17:05:00 -0500

    quinta version

--- dentiupdate.php
+++ dentiupdate.php
@@ -1,16 +1,22 @@
 <?php
   include 'denticonfig.php';
-  $name = filter_input(INPUT_POST, "nombre1");
-  $lastname = filter_input(INPUT_POST, "apellido1");
-  $birthday = filter_input(INPUT_POST, "nacimiento1");
-  $gender = filter_input(INPUT_POST, "sexo1");
-  $phone = filter_input(INPUT_POST, "telefono1");
-  $address = filter_input(INPUT_POST, "direccion1");
-  $query = "UPDATE PACIENTE SET NOMBRE_P='".$name."',APELLIDO_P='".$lastname."',SEXO='".$gender."',TELEFONO='".$phone."',DOMICILIO='".$address."',FECHA_NAC='".$birthday."'WHERE NOMBRE_P='".$name."'";
+  $name = "claudio";
+  $lastnameF = "Hernandez";
+  $lastnameM = "Ruiz";
+  $telefono = "3321647768";
+  $peso = "3kg";
+  $edad = "12";
+  /*$name = filter_input(INPUT_POST, "nombre1");
+  $lastnameF = filter_input(INPUT_POST, "apellidopaterno1");
+  $lastnameM = filter_input(INPUT_POST, "apellidomaterno1");
+  $telefono = filter_input(INPUT_POST, "telefono1");
+  $peso = filter_input(INPUT_POST, "peso1");
+  $edad = filter_input(INPUT_POST, "edad1");*/
+  $query = "UPDATE paciente SET nombre='".$name."',apellido_paterno='".$lastnameF."',apellido_materno='".$lastnameM."',telefono='".$telefono."',peso='".$peso."',edad='".$edad."'WHERE nombre='".$name."'";
   $result = mysqli_query($conn, $query);
   $lastId = mysqli_insert_id($conn);
 
-  $query3 = "SELECT * FROM paciente WHERE ID_PAC = '".$lastId."'";
+  $query3 = "SELECT * FROM paciente WHERE id_paciente = '".$lastId."'";
   $result3 = mysqli_query($conn, $query3);
   $row = mysqli_fetch_array($result3);
   $row = mysqli_fetch_array($result3);
