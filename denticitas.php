3bb81412e8010d6827538aeaacb5b1cfb96eac3f
Christofer-Luevanos <a17100195@gmail.com>
2020-06-21 16:16:03 -0500

    segunda version

--- denticitas.php
+++ denticitas.php
@@ -1,24 +1,45 @@
 <?php
   include 'denticonfig.php';
-  $name = filter_input(INPUT_POST, "nombre");
-  $lastname = filter_input(INPUT_POST, "apellido");
-  $doctor = filter_input(INPUT_POST, "doctor");
-  $date = filter_input(INPUT_POST, "fecha");
-  $hour = filter_input(INPUT_POST, "hora");
+  $nombre_us="Fernanda";
+  $name = "Claudio";
+  $lastnameF = "Hernandez";
+  $lastnameM = "Ruiz";
+  $telefono = "3330238313";
+  $edad = "18";
+  $peso = "1kg";
+  //$nombre_us=filter_input(INPUT_POST,"n_us")
+  //$name = filter_input(INPUT_POST, "nombre");
+  //$lastnameF = filter_input(INPUT_POST, "apellido_pat");
+  //$lastnameM = filter_input(INPUT_POST, "apellido_mat");
+  //$telefono = filter_input(INPUT_POST, "telefono");
+  //$edad = filter_input(INPUT_POST, "edad");
+  //$peso = filter_input(INPUT_POST, "peso");
 
-  $pacientQuery = "SELECT ID_PAC FROM paciente WHERE NOMBRE_P='".$name."' AND APELLIDO_P='".$lastname."'";
+  $pacientQuery = "SELECT id_paciente FROM paciente WHERE nombre='".$name."' AND apellido_paterno='".$lastnameF."'";
   $pacientResult = mysqli_query($conn, $pacientQuery);
   $pacientRow = mysqli_fetch_assoc($pacientResult);
+  echo $pacientRow["id_paciente"];
 
-$query = "INSERT INTO cita(ID_PAC, ID_DOC, FECHA_CITA, HORA_CITA)
-  VALUES ('".$pacientRow['ID_PAC']."','".$doctor."','".$date."','".$hour."')";
+  $pacientQuery2 = "SELECT id_usuario FROM usuario WHERE nombre='".$name."'";
+  $pacientResult2 = mysqli_query($conn, $pacientQuery2);
+  $pacientRow2 = mysqli_fetch_assoc($pacientResult2);
+  echo $pacientRow2["id_usuario"];
+
+  $query = "INSERT INTO paciente(nombre,apellido_paterno,apellido_materno,telefono,edad,peso)
+  VALUES ('".$name."','".$lastnameF."','".$lastnameM."','".$telefono."','".$edad."','".$peso."')";
   echo $query;
   $result = mysqli_query($conn, $query);
   $lastId = mysqli_insert_id($conn);
 
-  $query2 = "SELECT * FROM cita WHERE ID_CITA = '".$lastId."'";
+  $query2 = "INSERT INTO us_pa(id_usuario,id_paciente)
+  VALUES ('".$pacientRow2['id_usuario']."','".$pacientRow['id_paciente']."')";
+  echo $query2;
   $result2 = mysqli_query($conn, $query2);
-  $row = mysqli_fetch_array($result2);
+  $lastId2 = mysqli_insert_id($conn);
+
+  $query3 = "SELECT * FROM paciente WHERE id_paciente = '".$lastId."'";
+  $result3 = mysqli_query($conn, $query3);
+  $row = mysqli_fetch_array($result3);
   if($row){
     echo '1';
   }
--- dentingresos.php
+++ dentingresos.php
@@ -1,18 +1,19 @@
 <?php
   include 'denticonfig.php';
+  /*$name = "Fernanda";
+  $email = "12345@gmail.com";
+  $contra = "12345";*/
   $name = filter_input(INPUT_POST, "nombre");
-  $lastname = filter_input(INPUT_POST, "apellido");
-  $birthday = filter_input(INPUT_POST, "nacimiento");
-  $gender = filter_input(INPUT_POST, "sexo");
-  $phone = filter_input(INPUT_POST, "telefono");
-  $address = filter_input(INPUT_POST, "direccion");
+  $email = filter_input(INPUT_POST, "email");
+  $contra = filter_input(INPUT_POST, "contra");
 
-  $query = "INSERT INTO paciente(NOMBRE_P, APELLIDO_P, SEXO, TELEFONO, DOMICILIO, FECHA_NAC)
-  VALUES ('".$name."','".$lastname."','".$gender."','".$phone."','".$address."','".$birthday."')";
+
+  $query = "INSERT INTO usuario(nombre,email,contra) 
+  VALUES ('".$name."','".$email."','".$contra."')";
   $result = mysqli_query($conn, $query);
   $lastId = mysqli_insert_id($conn);
 
-  $query2 = "SELECT * FROM paciente WHERE ID_PAC = '".$lastId."'";
+  $query2 = "SELECT * FROM usuario WHERE Id_usuario = '".$lastId."'";
   $result2 = mysqli_query($conn, $query2);
   $row = mysqli_fetch_array($result2);
   if($row){
