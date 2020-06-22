2dfbcead30c17d6dbeae49bffb58e20d89be821c
Christofer-Luevanos <a17100195@gmail.com>
2020-06-22 00:38:47 -0500

    septimau version

--- iniciarcita.php
+++ iniciarcita.php
@@ -6,7 +6,7 @@ $menu = 1;
 $resultadoArray = array();
 
 if($menu == 1){
-  $mascotaQuery = "SELECT * FROM paciente WHERE id_upaciente > '$aux'";
+  $mascotaQuery = "SELECT nombre FROM paciente WHERE id_upaciente > '$aux'";
   $mascotaResultado = mysqli_query($conn, $mascotaQuery);
   foreach($mascotaResultado as $row){
     $resultadoArray[] = $row;
--- pacientes.php
+++ pacientes.php
@@ -3,7 +3,7 @@ header('Content-Type: application/json');
 include 'denticonfig.php';
 $aux=1;
 $resultadoArray = array();
-$pacienteQuery = "SELECT * FROM paciente WHERE id_paciente>'$aux'";
+$pacienteQuery = "SELECT nombre FROM paciente WHERE id_paciente>'$aux'";
 $pacienteResultado = mysqli_query($conn, $pacienteQuery);
 foreach($pacienteResultado as $row){
  $resultadoArray[] = $row;
